8e8f15887932546a8c0713b4a385de239685ab09
Sergey Luzik <luziksergey@gmail.com>
2025-04-21 06:28:17 +0300

    feat: adds validation to the AddTodo

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,10 +1,35 @@
+import { log } from "console"
 import styles from "./styles.module.css"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+interface IFormInput {
+  taskField: string
+}
 
 export const AddTodo = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<IFormInput>({
+    mode: "onChange",
+    defaultValues: { taskField: "" },
+  })
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data.taskField)
+  }
+  const validateTaskText = (value: string) => {
+    const lattersOnly = value.replace(/[^a-zA-Zа-яА-Я]/g, "")
+    return lattersOnly.length >= 2
+  }
   return (
-    <form className={styles.form}>
-      <input className={styles.input} placeholder="Новая задача" />
-      <button type="submit" className={styles.add}></button>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className={styles.input}
+        placeholder="Новая задача"
+        {...register("taskField", { validate: validateTaskText })}
+      />
+      <button type="submit" className={styles.add} disabled={!isValid}></button>
     </form>
   )
 }
