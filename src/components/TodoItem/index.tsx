7a2a12d41d9079e6a7f177dc2a498642985bee2b
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 15:13:32 +0300

    feat: adds Popup to TodoItem

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import type { Todo } from '../../store/todosSlice';
 import styles from './styles.module.css';
+import { Popup } from '../Popup';
 
 export const TodoItem = ({ todo }: { todo: Todo }) => {
   const { id, text, isDone } = todo;
+  const [showPopup, setShowPopup] = useState(false);
+  const togglePopup = (prevState: boolean) => setShowPopup(!prevState);
   return (
-    <li className={styles.item}>
-      <label className={styles.task}>
-        {text}
-        <input className={styles.checkbox} type="checkbox" defaultChecked={isDone} />
-      </label>
-      <button className={styles.remove}></button>
-    </li>
+    <>
+      <li className={styles.item}>
+        <label className={styles.task}>
+          {text}
+          <input className={styles.checkbox} type="checkbox" defaultChecked={isDone} />
+        </label>
+        <button className={styles.remove} onClick={togglePopup}></button>
+      </li>
+      {showPopup && <Popup todoName={text} />}
+    </>
   );
 };
