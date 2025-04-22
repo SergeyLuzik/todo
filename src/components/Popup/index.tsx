f88262643cf1c323b3a7c88489a362bd65a39477
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 21:13:51 +0300

    feat: adds styles and state handlers to Popup

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,23 +1,25 @@
 import { createPortal } from 'react-dom';
 import styles from './styles.module.css';
 
-export const Popup = ({ todoName }: { todoName: string }) =>
+type PopupProps = { todoName: string; togglePopup: () => void; onDeleteTodo: () => void };
+
+export const Popup = ({ todoName, togglePopup, onDeleteTodo }: PopupProps) =>
   createPortal(
     <div className={styles.popup}>
       <div className={styles.content}>
         <div className={styles.header}>
-          <p className={styles.title}>{`Вы действительно хотите удалить ${todoName}?`}</p>
-          <button className={styles.close} type="button"></button>
+          <p className={styles.title}>{`Вы действительно хотите удалить "${todoName}"?`}</p>
+          <button className={styles.close} type="button" onClick={togglePopup}></button>
         </div>
         <div className={styles.buttons}>
-          <button className={styles.remove} type="button">
+          <button className={styles.action} type="button" onClick={onDeleteTodo}>
             Да
           </button>
-          <button className={styles.cancel} type="button">
+          <button className={styles.action} type="button" onClick={togglePopup}>
             Нет
           </button>
         </div>
       </div>
     </div>,
-    document.body
+    document.getElementById('modal-root')
   );
