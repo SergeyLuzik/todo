2f86cd1514e7f15319516cbaf85c324243548380
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 06:18:42 +0300

    feat: adds selectors to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@
 // import { Counter } from "./features/counter/Counter"
 // import { Quotes } from "./features/quotes/Quotes"
 // import logo from "./logo.svg"
-import styles from "./App.module.css"
-import { AddTodo } from "./components/AddTodo"
-import { TodoList } from "./components/TodoList"
+import styles from './App.module.css';
+import { AddTodo } from './components/AddTodo';
+import { TodoList } from './components/TodoList';
+import { useAppSelector } from './store/hooks';
+import { selectCompletedTodos, selectTodosInProgress } from './store/todosSlice';
 
-export const App = () => (
-  <div className={styles.app}>
-    <AddTodo />
-    <TodoList />
-  </div>
-)
+export const App = () => {
+  const todosInProgress = useAppSelector(selectTodosInProgress);
+  const completedTodos = useAppSelector(selectCompletedTodos);
+  return (
+    <div className={styles.app}>
+      <AddTodo />
+      <TodoList title="Список дел" todos={todosInProgress} />
+      <TodoList title="Завершено" todos={completedTodos} />
+    </div>
+  );
+};
