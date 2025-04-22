cd85ac559c398ebfdc65df782525dc30db0d405d
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 06:17:19 +0300

    feat: updates AddTodo form code and adds dispatch

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,35 +1,49 @@
-import styles from "./styles.module.css"
-import type { SubmitHandler } from "react-hook-form"
-import { useForm } from "react-hook-form"
+import styles from './styles.module.css';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { useAppDispatch } from '../../store/hooks';
+import { todoAdded } from '../../store/todosSlice';
+import type React from 'react';
 
 type FormInput = {
-  taskField: string
-}
+  taskField: string;
+};
+
+type AddTodoFormElement = {
+  readonly taskField: HTMLInputElement;
+} & HTMLFormElement;
 
 export const AddTodo = () => {
+  const dispatch = useAppDispatch();
   const {
     register,
     handleSubmit,
     formState: { isValid },
+    reset
   } = useForm<FormInput>({
-    mode: "onChange",
-    defaultValues: { taskField: "" },
-  })
-  const onSubmit: SubmitHandler<FormInput> = data => {
-    console.log(data.taskField)
-  }
+    mode: 'onChange',
+    defaultValues: { taskField: '' }
+  });
+  const onSubmit: SubmitHandler<FormInput> = (
+    { taskField },
+    e: React.FormEvent<AddTodoFormElement>
+  ) => {
+    e.preventDefault();
+    dispatch(todoAdded({ text: taskField }));
+    reset();
+  };
   const validateTaskText = (value: string) => {
-    const lattersOnly = value.replace(/[^a-zA-Zа-яА-Я]/g, "")
-    return lattersOnly.length >= 2
-  }
+    const lattersOnly = value.replace(/[^a-zA-Zа-яА-Я]/g, '');
+    return lattersOnly.length >= 2;
+  };
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <input
         className={styles.input}
         placeholder="Новая задача"
-        {...register("taskField", { validate: validateTaskText })}
+        {...register('taskField', { required: true, validate: validateTaskText })}
       />
       <button type="submit" className={styles.add} disabled={!isValid}></button>
     </form>
-  )
-}
+  );
+};
