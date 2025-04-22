1bb9dc3caa429107b6d4cb79ff5b489373e370b5
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 06:14:38 +0300

    feat: adds todosSlice

---
+++ src/store/todosSlice.ts
@@ -0,0 +1,45 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+export type Todo = {
+  id: string;
+  text: string;
+  isDone: boolean;
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [] as Todo[],
+  reducers: {
+    todoAdded(state, action: PayloadAction<{ text: string }>) {
+      const todoText = action.payload.text;
+      const newTodo: Todo = {
+        id: nanoid(),
+        text: todoText,
+        isDone: false
+      };
+      state.push(newTodo);
+    },
+    todoDeleted(state, action: PayloadAction<{ id: string }>) {
+      const todoId = action.payload.id;
+      return state.filter((todo) => todo.id !== todoId);
+    },
+    todoStatusUpdated(state, action: PayloadAction<{ id: string; isDone: boolean }>) {
+      const { id, isDone } = action.payload;
+      const todo = state.find((todo) => todo.id === id);
+      if (todo) {
+        todo.isDone = isDone;
+      }
+    }
+  },
+  selectors: {
+    selectTodosInProgress: (todos) => todos.filter((todo) => !todo.isDone),
+    selectCompletedTodos: (todos) => todos.filter((todo) => todo.isDone)
+  }
+});
+
+export const { todoAdded, todoDeleted, todoStatusUpdated } = todosSlice.actions;
+
+export const { selectTodosInProgress, selectCompletedTodos } = todosSlice.selectors;
+
+export default todosSlice.reducer;
