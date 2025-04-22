bc8b4a902d6de233685629923fcf439ebb6456db
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 15:13:11 +0300

    feat: adds Popup markup

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,1 +1,23 @@
-import styles from "./styles.module.css"
+import { createPortal } from 'react-dom';
+import styles from './styles.module.css';
+
+export const Popup = ({ todoName }: { todoName: string }) =>
+  createPortal(
+    <div className={styles.popup}>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <p className={styles.title}>{`Вы действительно хотите удалить ${todoName}?`}</p>
+          <button className={styles.close} type="button"></button>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.remove} type="button">
+            Да
+          </button>
+          <button className={styles.cancel} type="button">
+            Нет
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
