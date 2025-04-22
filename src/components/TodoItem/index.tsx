bdfdfc0ea216583818f3d82a784a06c6f486e79b
Sergey Luzik <luziksergey@gmail.com>
2025-04-22 06:18:16 +0300

    feat: adds props to TodoItem

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,13 +1,15 @@
-import styles from "./styles.module.css"
+import type { Todo } from '../../store/todosSlice';
+import styles from './styles.module.css';
 
-export const TodoItem = () => {
+export const TodoItem = ({ todo }: { todo: Todo }) => {
+  const { id, text, isDone } = todo;
   return (
     <li className={styles.item}>
       <label className={styles.task}>
-        Do the dishes
-        <input className={styles.checkbox} type="checkbox" />
+        {text}
+        <input className={styles.checkbox} type="checkbox" defaultChecked={isDone} />
       </label>
       <button className={styles.remove}></button>
     </li>
-  )
-}
+  );
+};
